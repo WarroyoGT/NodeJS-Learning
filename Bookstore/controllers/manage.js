470f57fa53ce61b928a76521d6695fa5e377c5e7
warroyo <walter.arroyo@xoom.com>
2016-02-04 14:22:06 -0600

    Add book feature added

--- Bookstore/controllers/manage.js
+++ Bookstore/controllers/manage.js
@@ -3,12 +3,12 @@
 var Book = require('../models/bookModel');
 var Category = require('../models/categoryModel');
 
-module.exports = function (router) {
-    router.get('/', function (req, res) {
+module.exports = function(router) {
+    router.get('/', function(req, res) {
         res.render('manage/index');
     });
-    router.get('/books', function (req, res) {
-        Book.find({}, function (err, books) {
+    router.get('/books', function(req, res) {
+        Book.find({}, function(err, books) {
             if (err) {
                 console.log(err);
             }
@@ -16,11 +16,11 @@ module.exports = function (router) {
             var model = {
                 books: books
             };
-            res.render('manage/books/index',model);
+            res.render('manage/books/index', model);
         });
     });
-    router.get('/categories', function (req, res) {
-        Category.find({}, function (err, categories) {
+    router.get('/categories', function(req, res) {
+        Category.find({}, function(err, categories) {
             if (err) {
                 console.log(err);
             }
@@ -28,8 +28,64 @@ module.exports = function (router) {
             var model = {
                 categories: categories
             };
-            res.render('manage/categories/index',model);
+            res.render('manage/categories/index', model);
         });
 
+    });
+
+    router.get('/books/add', function(req, res) {
+
+        Category.find({}, function(err, categories) {
+            if (err) {
+                console.log(err);
+            }
+
+            var model = {
+                categories: categories
+            };
+            res.render('manage/books/add', model);
+        });
+    });
+
+    router.post('/books',function (req,res) {
+
+        var title = req.body.title && req.body.title.trim();
+        var category = req.body.category && req.body.category.trim();
+        var author = req.body.author && req.body.author.trim();
+        var publisher = req.body.publisher && req.body.publisher.trim();
+        var price = req.body.price && req.body.price.trim();
+        var description = req.body.description && req.body.description.trim();
+        var cover = req.body.cover && req.body.cover.trim();
+
+        if (title =='' || price =='') {
+            req.flash('error','Please fill out required fields');
+            res.location('/manage/books/add');
+            res.redirect('/manage/books/add');
+        }
+
+        if (isNaN(price)) {
+            req.flash('error','Price must be a number');
+            res.location('/manage/books/add');
+            res.redirect('/manage/books/add');
+        }
+
+        var newBook = new Book({
+            title:title,
+            category: category,
+            description: description,
+            author: author,
+            publisher: publisher,
+            cover: cover,
+            price: price
+        });
+        
+        newBook.save( function (err) {
+            if (err) {
+                console.log(err);
+            }
+            req.flash('success','Book Added');
+            res.location('/manage/books');
+            res.redirect('/manage/books ');
+        })
     });
 };
